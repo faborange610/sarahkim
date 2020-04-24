fcb2ca3c4fe768069cd8e6306053f3f2f32ea9f3
Songeun Kim <faborange610@gmail.com>
2020-04-24 21:17:19 +0900

    Initial Commit

---
+++ Finden/EventsFeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EventsFeedViewController.swift
+//  Finden
+//
+//  Created by 김송은 on 4/24/20.
+//  Copyright © 2020 Finden. All rights reserved.
+//
+
+import UIKit
+
+class EventsFeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+
+    @IBOutlet weak var eventsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "row: \(indexPath.row)"
+        
+        return cell
+    }
+
+}
